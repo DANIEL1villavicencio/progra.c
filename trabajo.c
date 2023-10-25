340caf2f3f38f34ec64e5bbd2a6b4095e72df3f4
DANIEL1villavicencio <146488697+DANIEL1villavicencio@users.noreply.github.com>
2023-10-25 14:26:49 -0500

    Update trabajo.c

--- trabajo.c
+++ trabajo.c
@@ -1,32 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-
-float volumenf(float cx, float cy, float cz, float rx, float ry, float rz, float altura);
+#include "volumen.h"
 
 int main(void)
 
 {
     float rx = 0, ry = 0, rz = 0, cx = 0, cy = 0, cz = 0, altura = 0, numd = 0, volumen = 0, radio;
-
-    printf("ingrese el centro del cono, como coordenadas x,y,z\n");
-    scanf("%f %f %f", &cx, &cy, &cz);
-    printf("ingrese el radio del cono,como coordenadas xyz\n");
-    scanf("%f %f %f", &rx, &ry, &rz);
-    printf("ingrese la altura del cono\n");
-    scanf("%f", &altura);
+    cx = datos();
+    cy = datos();
+    cz = datos();
+    rx = datos();
+    ry = datos();
+    rz = datos();
+    altura= datos();
     volumen = volumenf(cx, cy, cz, rx, ry, rz, altura);
-    printf("el volumen del cono truncado es %f\n", &volumen);
+    printf("el volumen del cono truncado es %f\n", volumen);
 
     return 0;
-}
-
-float volumenf(float cx, float cy, float cz, float rx, float ry, float rz, float altura)
-{
-    float vol, alturaf, radio, radiot;
-    alturaf = altura * 1 / 4;
-    radio = sqrt(pow(rx - cx, 2) + pow(ry - cy, 2) + pow(rz - cz, 2));
-    radiot = (1/4) * radio;
-    vol = ((pow(radio, 2) * altura * 3.14) / 3) - ((pow(radiot, 2) * alturaf * 3.14) / 3);
-
-    return vol;
 }
