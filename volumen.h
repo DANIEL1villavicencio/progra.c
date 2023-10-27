af2abc46810246ffa24441db8eea02381c5f3346
DANIEL1villavicencio <146488697+DANIEL1villavicencio@users.noreply.github.com>
2023-10-27 15:54:31 -0500

    volumen.h

--- volumen.h
+++ volumen.h
@@ -1,2 +1,2 @@
-float volumenf(float cx, float cy, float cz, float rx, float ry, float rz, float altura);
-float datos();
+float volumenf(float cx, float cy, float cz, float rx, float ry, float rz, float a1,float a2, float a3,float r2x, float r2y, float r2z);
+float datos(char centro[20], char coordenada[20]);
